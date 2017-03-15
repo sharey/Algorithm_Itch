236a0ed30ca081608a054d3533f8cf2adffa0c2f
Shraddha <swaghmode@ShraddhfficeMac.cudanet.local>
2017-03-15 12:21:48 -0700

    Longest non repeated substring

---
+++ find_longest_nonrepeated_substring.php
@@ -0,0 +1,33 @@
+# Find the longest non repeated substring length within the given string
+----------------------------------------------------------------------
+<?php
+
+$given_string = (!empty($argv[1]) ? $argv[1] : "abcdefgabdcstuvwxyz");
+
+if(empty($given_string))
+    die("Invalid String");
+
+$max_length         = 0;
+$max_length_string  = "";
+$arr_letters_visited= array();
+
+// One pointer i to iterate through all letters in given string and put it in visited array
+for($i= $j = 0; $i<strlen($given_string); $i++)
+{
+    // If current letter is found in visited array, increment second pointer j to next of visited letter's index
+    if(array_key_exists($given_string[$i], $arr_letters_visited))
+        $j = max($j, $arr_letters_visited[$given_string[$i]]+1);
+    
+    $arr_letters_visited[$given_string[$i]] = $i; // Replace visited index to new
+    
+    // Calculate max length and string here
+    if($max_length < $i-$j+1)
+    {
+        $max_length = $i-$j+1;
+        $max_length_string = substr($given_string, $j, $max_length);
+    }
+}
+
+echo "\r\nLongest non repeated substring of given string '".$given_string."' is: ".$max_length." (".$max_length_string.")\r\n";
+
+
